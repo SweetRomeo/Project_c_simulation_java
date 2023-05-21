f753e5516380357be2533d7fe6309b668e6b5f91
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2023-05-21 21:04:08 +0300

    Update Test.c

--- .github/workflows/Test.c
+++ .github/workflows/Test.c
@@ -1,62 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <limits.h>
 #include <string.h>
-#include "Oyun.h"
-#include "Taktik.h"
-#include "Koloni.h"
 #include "Uretim.h"
 
-#define randomize() srand(time(NULL))
-#define SIZE 100
+#include "Taktik.h"
+#include "Koloni.h"
+#include "Oyun.h"
 
 int powOf10(int num)
 {
     int res = 1;
     int i;
     for (i = 2; i <= num; i++)
+    {
         res *= 10;
+    }
     return res;
 }
 
-int strToNum(char *str, int num) {
+
+
+int strToNum(char *str, int num)
+{
     int i, res = 0;
     for(i = num-1; i >= 0; i--)
-        res += (int)(str[i] - '0')*powOf10(num - i);
+    {
+        res += (int) (str[i] - '0') * powOf10(num - i);
+    }
     return res;
 }
 
 
-int* readInputBuffer(char* str, char ch, int* stringSize, int* spaceCount, int* lastSpaceFlag)
+int* readInputBuffer(char* str, int* stringSize, int* numberOfColonies)
 {
     fgets(str, 100, stdin);
     *stringSize = strlen(str);
-    (*stringSize)++;
 
-    *spaceCount = 0;
-    for (int i = 0; i < *stringSize; i++) {
-        if (str[i] == ' ') {
-            (*spaceCount)++;
+    *numberOfColonies = 0;
+    for (int i = 0; i < *stringSize; i++)
+    {
+        if (str[i] == ' ')
+        {
+            (*numberOfColonies)++;
         }
     }
-    (*spaceCount)++;
+    (*numberOfColonies)++;
     printf("%d\n", strToNum(str, 4));
-    int* numdatasarr = (int*)malloc(sizeof(int) * (*spaceCount));
+    int* numdatasarr = (int*)malloc(sizeof(int) * (*numberOfColonies));
     int numlen = 0, koloni_index = 0;
 
-    for (int i = 0; i < *stringSize; i++) {
+    for (int i = 0; i < *stringSize; i++)
+    {
         numlen++;
-        if (str[i] == ' ') {
+        if (str[i] == ' ')
+        {
             numdatasarr[koloni_index] = strToNum(str + i - numlen + 1, numlen - 1);
             printf("str: %c\n", *(str + i - numlen + 1));
             printf("index: %d\n", i);
-            //printf("numdatasarr : %d ",numdatasarr[koloni_index] );
             koloni_index++;
             numlen = 0;
         }
         if(i == (*stringSize)-1)
+        {
             numdatasarr[koloni_index] = strToNum(str + i - numlen + 1, numlen - 1);
+        }
 
     }
 
@@ -64,90 +72,111 @@ int* readInputBuffer(char* str, char ch, int* stringSize, int* spaceCount, int* 
 }
 
 
+int randomInterval(int interval1, int interval2)
+{
+    return interval1 > interval2 ? (rand()%interval1 + interval2) : (rand()%interval2 + interval1);
+}
+
+
 int main()
 {
-    char str[SIZE];
-    char ch = 0;
+    char str[100];
     int stringSize = 0;
-    int spaceCount = 0, lastSpaceFlag = 0;
-    int *numdatasarr = readInputBuffer(str, ch, &stringSize, &spaceCount, &lastSpaceFlag );
-    printf("%d",*numdatasarr);
-    str[stringSize] = ' ';
-    str[stringSize + 1] = '\0';
-    printf("\n");
-    printf("Input string: %s\n", str);
-    printf("Number of spaces: %d\n", spaceCount);
-    int numberOfColonies = ++spaceCount;
+    int numberOfColonies = 0;
+    int *numdatasarr = readInputBuffer(str, &stringSize, &numberOfColonies);
     Colony* colonies = (Colony*)malloc(sizeof(Colony) * numberOfColonies);
+    printf("%d", numdatasarr[numberOfColonies - 1]);
     for (int i = 0; i < numberOfColonies; ++i)
         createColonyTest(&colonies[i], numdatasarr);
-    int deathCount = 0;
-    int turnCount = 1;
-    while (1) {
+    int deathcolonienumber = 0;
+    int turnnumber = 1;
+    srand(time(NULL));
+    while (deathcolonienumber < numberOfColonies - 1)
+    {
         printf("----------------------------------------------------------------------------\n");
-        printf("Turn Count: %d\n", turnCount);
+        printf("Number of Turn: %d\n", turnnumber);
         printf("Colony    Population   Food Stock     Wins       Losses\n");
-        turnCount++;
-        for (int i = 0; i < spaceCount; i++) {
-            if (!colonies[i]->isDead && (colonies[i]->population <= 0 || colonies[i]->foodStock <= 0)) {
-                deathCount++;
+        turnnumber++;
+        for (int i = 0; i < numberOfColonies; i++) {
+            if (!colonies[i]->isDead && (colonies[i]->population <= 0 || colonies[i]->foodStock <= 0))
+            {
+                deathcolonienumber++;
                 colonies[i]->isDead = 1;
-                printf("%d died.\n", i);
             }
-            if (!colonies[i]->isDead)
+            if (!colonies[i]->isDead) {
                 printf("%4c %8d %13d %13d %13d\n", colonies[i]->symbol, colonies[i]->population, colonies[i]->foodStock, colonies[i]->wins, colonies[i]->losses);
+            }
             else
-                printf("%4c      --         --         --        --\n", colonies[i]->symbol);
+            {
+                printf("%4c      --             --            --            --\n", colonies[i]->symbol);
+            }
         }
         printf("----------------------------------------------------------------------------\n");
+        int Battleresults[2];
+        for (int i = 0; i < numberOfColonies - 1; i++)
+        {
+            for (int j = i + 1; j < numberOfColonies; j++)
+            {
+                if (colonies[i]->isDead == 0 && colonies[j]->isDead == 0)
+                {
+                    Battleresults[0] = Savas();
+                    Battleresults[1] = Savas();
 
-        for (int i = 0; i < spaceCount - 1; i++) {
-            for (int j = i + 1; j < spaceCount; j++) {
-                if (!(colonies[i]->isDead || colonies[j]->isDead)) {
-                    int tempBattle1 = Savas(); // Colony i
-                    int tempBattle2 = Savas(); // Colony j
-                    int winner;
-                    int foodTransferTemp;
-
-                    if (tempBattle1 == tempBattle2) {
+                    if (Battleresults[0] < Battleresults[1])
+                    {
+                        colonies[i]->population -= colonies[i]->population * ((int)((Battleresults[1] - Battleresults[0]) / 1000.));
+                        colonies[j]->foodStock += (int)((colonies[i]->foodStock / 100.) * 10);
+                        colonies[i]->foodStock -= (int)((colonies[j]->foodStock / 100.) * 10);
+                        colonies[i]->losses = colonies[i]->losses + 1;
+                        colonies[j]->wins = colonies[j]->wins + 1;
+                    } else if (Battleresults[0] > Battleresults[1])
+                    {
+                        colonies[j]->population -= ((int)((Battleresults[0] - Battleresults[1]) / 1000.)) * colonies[j]->population;
+                        colonies[i]->foodStock += (int)(10 * (colonies[j]->foodStock / 100.));
+                        colonies[j]->foodStock -= (int)(10 * (colonies[i]->foodStock / 100.));
+                        colonies[i]->wins = colonies[i]->wins + 1;
+                        colonies[j]->losses = colonies[j]->losses + 1;
+                    } else if (Battleresults[0] == Battleresults[1])
+                    {
                         if (colonies[i]->population == colonies[j]->population)
-                            winner = rand() % 2 + 1;
-                        else if (colonies[i]->population > colonies[j]->population)
-                            winner = 1;
-                        else
-                            winner = 2;
-                    } else if (tempBattle1 > tempBattle2) {
-                        winner = 1;
-                    } else {
-                        winner = 2;
-                    }
+                        {
+                            if (randomInterval(1, 2) == 1)
+                            {
+                                colonies[i]->foodStock += (int) (10 * (colonies[j]->foodStock / 100.));
+                                colonies[j]->foodStock -= (int) (10 * (colonies[i]->foodStock / 100.));
+                                colonies[i]->wins = colonies[i]->wins + 1;
+                                colonies[j]->losses = colonies[j]->losses + 1;
+                            } else
+                            {
+                                colonies[j]->foodStock += (int) ((colonies[i]->foodStock / 100.) * 10);
+                                colonies[i]->foodStock -= (int) ((colonies[i]->foodStock / 100.) * 10);
+                                colonies[i]->losses = colonies[i]->losses + 1;
+                                colonies[j]->wins = colonies[j]->wins + 1;
+                            }
+                        } else if (colonies[i]->population > colonies[j]->population)
+                        {
+                            colonies[i]->foodStock += (int) (10 * (colonies[j]->foodStock / 100.));
+                            colonies[j]->foodStock -= (int) (10 * (colonies[i]->foodStock / 100.));
+                            colonies[i]->wins = colonies[i]->wins + 1;
+                            colonies[j]->losses = colonies[j]->losses + 1;
+                        } else
+                        {
+                            colonies[j]->foodStock += (int) ((colonies[i]->foodStock / 100.) * 10);
+                            colonies[i]->foodStock -= (int) ((colonies[j]->foodStock / 100.) * 10);
+                            colonies[i]->losses = colonies[i]->losses + 1;
+                            colonies[j]->wins = colonies[j]->wins + 1;
+                        }
 
-                    if (winner == 1) {
-                        colonies[i]->wins++;
-                        colonies[j]->losses++;
-                        colonies[j]->population -= colonies[j]->population * ((int)((double)(tempBattle1 - tempBattle2) / 1000));
-                        foodTransferTemp = (int)(((double)colonies[j]->foodStock / 100) * 10);
-                        colonies[i]->foodStock += foodTransferTemp;
-                        colonies[j]->foodStock -= foodTransferTemp;
-                    } else {
-                        colonies[i]->losses++;
-                        colonies[j]->wins++;
-                        colonies[i]->population -= colonies[i]->population * ((int)((double)(tempBattle2 - tempBattle1) / 1000));
-                        foodTransferTemp = (int)(((double)colonies[i]->foodStock / 100) * 10);
-                        colonies[j]->foodStock += foodTransferTemp;
-                        colonies[i]->foodStock -= foodTransferTemp;
                     }
                 }
             }
         }
 
-        for (int i = 0; i < spaceCount; i++) {
-            colonies[i]->population += (int)(((double)colonies[i]->population / 100) * 20);
-            colonies[i]->foodStock -= colonies[i]->population * 2;
+        for (int i = 0; i < numberOfColonies; i++)
+        {
+            colonies[i]->population = (int)(colonies[i]->population*1.2);
+            colonies[i]->foodStock = colonies[i]->foodStock - colonies[i]->population * 2;
         }
-
-        if (deathCount >= spaceCount - 1)
-            break;
 
 
     }
